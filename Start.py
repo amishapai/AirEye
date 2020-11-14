950b02c42fa114efccc6e5c66806039eb5f838a1
amishapai <63700664+amishapai@users.noreply.github.com>
2020-11-14 19:31:30 +0530

    Update Start.py

--- Start.py
+++ Start.py
@@ -1,42 +1,23 @@
+# Start.py - Reads the configuration file and reads the AirEye configurations
+
+# Import modules
+
 import sys
 import os
 import datetime
+import logging
 
+# Define global variables
 
 global error_file_exists
 global module_config
 global aireye_config
+#global all_lines
 
 log_file_exists = 0
 error_file_exists = 0
 
 # Define functions
-
-def log_me(log_name, log_no, log_msg):
-    global log_file_exists
-    if log_file_exists == 0:
-        if os.path.exists('log.txt') == False:
-            log_file_exists = 1
-            file = open('log.txt', 'a+')
-            file.close()
-
-    file = open('log.txt', 'a+')
-    file.write(str(datetime.datetime.now()) + ":" + log_name + ":" + log_no + ":" + log_msg + "\n")
-    #file.write(datetime.datetime.now() + ":" + log_name + ":" + log_no + ":" + log_msg)
-    file.close()
-
-def write_error(error_name, error_no, error_msg):
-    global error_file_exists
-    error_file_name = 'error.txt'
-    if error_file_exists == 0:
-        if os.path.exists(error_file_name) == False:
-            error_file_exists = 1
-            file = open(error_file_name, 'a+')
-            file.close()
-
-    file = open(error_file_name, 'a+')
-    file.write(str(datetime.datetime.now()) + ":" + error_name + ":" + error_no + ":" + error_msg + "\n")
-    file.close()
 
 def List_Search(func_List, func_String):
     func_Result = 0
@@ -49,25 +30,55 @@ def List_Search(func_List, func_String):
     else:
                 return -1
 
-def config_check():
-    if os.path.exists('Config_File-2.txt') == False:
-        print ("0001: Error booting. Please try again or call a helpline.")
-        log_me('Config Error','0001','Config File does not exist')
-        write_error('Config Error','0001', 'Config File does not exist.')
+def logMe(log_name, log_no, log_msg):
+    global log_file_exists
+    if log_file_exists == 0:
+        if os.path.exists('log.txt') == False:
+            log_file_exists = 1
+            file = open('log.txt', 'a+')
+            file.close()
 
-def read_AirEye():
+    file = open('log.txt', 'a+')
+    file.write(str(datetime.datetime.now()) + ":" + log_name + ":" + log_no + ":" + log_msg + "\n")
+    #file.write(datetime.datetime.now() + ":" + log_name + ":" + log_no + ":" + log_msg)
+    file.close()
+
+def writeError(error_name, error_no, error_msg):
+    global error_file_exists
+    error_file_name = 'error.txt'
+    if error_file_exists == 0:
+        if os.path.exists(error_file_name) == False:
+            error_file_exists = 1
+            file = open(error_file_name, 'a+')
+            file.close()
+
+    file = open(error_file_name, 'a+')
+    file.write(str(datetime.datetime.now()) + ":" + error_name + ":" + error_no + ":" + error_msg + "\n")
+    file.close()
+
+def configCheck():
+    if os.path.exists('Config_File-2.txt') == False:
+        print ("0: Error booting. Please try again or call a helpline.")
+        log_me('Config Error','0','Config File does not exist')
+        writeError('Config Error','0', 'Config File does not exist.')
+    else:
+        logMe('Config Exists','1','Configuration file exists.')
+
+def readAirEye():
     file = open('Config_File-2.txt','r')
     all_lines = file.readlines()
     start = List_Search(all_lines, '<AirEye>')
     end = List_Search(all_lines, '</AirEye>')
+    start = start + 1
+    end = end - 1
     aireye = all_lines[start:end]
-    name = aireye[1]
-    number = aireye[2]
+    device_name = aireye[0]
+    device_number = aireye[1]
     file.close()
-    print (name)
-    print (number)
+    print (device_name)
+    print (device_number)
 
-def sense_timer():
-    while 1 == 1:
-        sense()
-        time.sleep(900)
+# Run functions
+
+readAirEye()
+configCheck()
